96f3474c45114c1149400dd94a559e728698ac5d
R Ram Shankar <110959980+ram-shankar58@users.noreply.github.com>
2023-03-06 15:08:45 +0000

    python file for control added

---
+++ onlinecodespace.py
@@ -0,0 +1,3 @@
+#This is the online codespace for adding changes to the document.
+print("This is the online codespace for adding changes to the document.")
+print("For more details, look at the menu and or the links that are to be posted soon.")
